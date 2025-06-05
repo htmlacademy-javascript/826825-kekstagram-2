bfed54cf88a878362e1a5e76b2af043ecc379b1b
Andrey <i3hanesy@yandex.ru>
2025-06-05 19:50:29 +0500

    Добавляет console log во второе задание

--- js/functions.js
+++ js/functions.js
@@ -4,8 +4,10 @@ function checksLengthString (checkString, maxLength) {
   return checkString.length <= maxLength;
 }
 
+console.log(checksLengthString('Пропоганда', 15));
+
 function checksPalindromeFirst (checkString) {
-  const normalasedCheckString = checkString.replaceAll().toUpperCase();
+  const normalasedCheckString = checkString.replaceAll(' ','').toUpperCase();
   let reverseString = '';
   for (let i = normalasedCheckString.length - 1; i > -1; i--) {
     reverseString += normalasedCheckString[i];
@@ -13,11 +15,15 @@ function checksPalindromeFirst (checkString) {
   return reverseString === normalasedCheckString;
 }
 
+console.log(checksPalindromeFirst('А роза упала на лапу Азора'));
+
 function checksPalindrome (checkString) {
-  const normalasedCheckString = checkString.replaceAll().toUpperCase();
+  const normalasedCheckString = checkString.replaceAll(' ','').toUpperCase();
   const reverseString = normalasedCheckString.split('').reverse().join('');
   return reverseString === normalasedCheckString;
 }
+
+console.log(checksPalindrome('А роза упала на лапу Азора'));
 
 function getNumberFromString (checkString) {
   const normalString = checkString.toString();
@@ -29,3 +35,5 @@ function getNumberFromString (checkString) {
   }
   return result ? result : NaN;
 }
+
+console.log(getNumberFromString('524 проблемы и 38 решений'));
