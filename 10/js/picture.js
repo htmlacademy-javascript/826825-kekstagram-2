54e89cbc56d23d7d399828a1741970ba7917fc68
Keks <keks@htmlacademy.ru>
2025-07-08 22:36:10 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/picture.js
@@ -0,0 +1,36 @@
+import {createPhotos} from './data.js';
+import {openBigPicture} from './big-picture.js';
+
+const picturesWrapper = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const userPhotos = createPhotos();
+
+const userPhotosFragment = document.createDocumentFragment();
+
+userPhotos.forEach(({id, url, description, likes, comments}) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  const image = pictureElement.querySelector('.picture__img');
+
+  pictureElement.dataset.id = id;
+  image.src = url;
+  image.alt = description;
+  pictureElement.querySelector('.picture__likes')
+    .textContent = likes;
+  pictureElement.querySelector('.picture__comments')
+    .textContent = comments.length;
+  userPhotosFragment.appendChild(pictureElement);
+});
+
+picturesWrapper.appendChild(userPhotosFragment);
+
+picturesWrapper.addEventListener('click', (evt) => {
+  const target = evt.target.closest('.picture');
+  if (!target) {
+    return;
+  }
+  const photoElement = userPhotos.find((photo) => photo.id === Number(target.dataset.id));
+  openBigPicture(photoElement);
+});
